31322c4c83212633266ea5bead4f99a132e294c2
wogus0518 <wogus0518@naver.com>
2022-02-02 22:59:32 +0900

    Chapter7

--- src/JAVA_Example/Ex7_27.java
+++ src/JAVA_Example/Ex7_27.java
@@ -1,46 +1,47 @@
 package JAVA_Example;
 
 class Product {
-    int price;
-    int bonusPoint;
+    private int price;
+    private int bonusPoint;
+
+    int getBonusPoint(){return bonusPoint; }
+    int getPrice(){ return price; }
 
     Product(){}
     Product(int price){
-        this.price = price;
-        this.bonusPoint = (int) (price*0.1);
+        if(price>0){
+            this.price = price;
+            this.bonusPoint = (int) (price*0.1);
+        }
     }
 }
-
-class Tv1 extends Product{
+class Tv1 extends Product {
     Tv1(int price){ super(price); }
     public String toString() { return "TV"; }
 }
-
-class Computer extends Product{
+class Computer extends Product {
     Computer(int price){ super(price); }
     public String toString() { return "Computer"; }
 }
 
-class Buyer{
+class Buyer {
     private int money = 1000;
-    private int bonusPoint = 0;
+    private int bonusPoint;
 
+    int getBonusPoint(){return bonusPoint; }
     int getMoney(){ return money; }
-    int getBonusPoint(){ return bonusPoint; }
 
     void buy(Product p){
-        if(money < p.price){
+        if(p.getPrice()>money){
             System.out.println("잔액이 부족합니다.");
-            return;
         }
 
-        money -= p.price;
-        bonusPoint += p.bonusPoint;
+        money -= p.getPrice();
+        bonusPoint += p.getBonusPoint();
         System.out.println(p.toString() + "을/를 구입하셨습니다.");
     }
 }
-
-public class Ex7_27 {
+public class Ex7_27{
     public static void main(String[] args) {
         Buyer b = new Buyer();
         Product t = new Tv1(100);
@@ -48,8 +49,8 @@ public class Ex7_27 {
 
         b.buy(t);
         b.buy(c);
-
         System.out.println("현재 남은 돈은" + b.getMoney() + "만원입니다.");
         System.out.println("현재 보너스점수는" + b.getBonusPoint() + "점입니다.");
+
     }
 }
